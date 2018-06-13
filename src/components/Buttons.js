10ff77483df5804e772847e5b62a32c456da4f59
Joose Helle <joose.helle@helsinki.fi>
2018-06-13 08:15:04 +0300

    Add styled navlinks & buttons

---
+++ src/components/Buttons.js
@@ -0,0 +1,80 @@
+import styled, { css } from 'styled-components'
+import { NavLink } from 'react-router-dom'
+
+
+const activeClassName = 'active'
+export const StyledNavLink = styled(NavLink).attrs({
+  activeClassName
+})`
+  border: 1px solid rgba(255, 255, 255, 0.9);
+  border-radius: 5px;
+  color: white;
+  cursor: pointer;
+  display: block;
+  font-size: 13px;
+  font-weight: 350;
+  letter-spacing: 1px;
+  margin: 7px 4px;
+  padding: 6px 13px;
+  text-align: center;
+  text-decoration: none;
+  transition-duration: 0.2s;
+  -webkit-transition-duration: 0.2s; /* Safari */
+  &:hover { 
+    border-color: white;
+    box-shadow: inset 0 0 0 0.5px white;  
+  }
+  &.${activeClassName} {
+    border-color: white;
+    box-shadow: inset 0 0 0 0.5px white;  
+  }
+  @media (max-width: 370px) {
+    font-size: 12px;
+    padding: 4px 7px;
+  }
+`
+
+
+
+export const Button = styled.button`
+  background: transparent;
+  border: 1px solid rgba(255, 255, 255, 0.9);
+  border-radius: 5px;
+  color: white;
+  cursor: pointer;
+  display: inline-block;
+  font-size: 13px;
+  font-weight: 300;
+  margin: auto;
+  padding: 6px 13px;
+  text-align: center;
+  text-decoration: none;
+  transition-duration: 0.2s;
+  -webkit-transition-duration: 0.2s; /* Safari */
+  @media (max-width: 370px) {
+    font-size: 12px;
+    padding: 5px 8px;
+  }
+${props => props.submit && css`
+  &:hover { 
+    border-color: white;
+    box-shadow: inset 0 0 0 0.5px white;  
+  }
+`}
+${props => props.signup && css`
+  border: none;
+  color: white;
+  margin-left: 10px;
+  &:hover { 
+    box-shadow: inset 0 0 0 0.5px white;  
+  }
+`}
+${props => props.cancel && css`
+  border: none;
+  color: #f5c77d;
+  font-size: 12px;
+  &:hover { 
+    box-shadow: inset 0 0 0 0.5px #f5c77d; 
+  } 
+`}
+`
